0fe34d38db1231c2e11d09ff8a62482648e8069e
adarshraut493 <adarshraut493@gmail.com>
2023-12-29 16:50:00 +0530

    ignore this commit

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -24,7 +24,7 @@ async function Page() {
             </p>
             <section className="mt-9 bg-dark-2 p-10">
                 <AccountProfile
-                    user={userData} btnTitle="Continue" />
+                    user={userData} btnTitle="Continue"/>
             </section>
 
         </main>
